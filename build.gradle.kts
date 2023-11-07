ca55ae33b95d68dae8798442e6219235a31b350a
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-11-07 09:39:08 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.17"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "co.edu.unisabana"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
