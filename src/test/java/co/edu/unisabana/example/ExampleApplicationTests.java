f94803d245581097fb16f0c176ab8d6454830888
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-11-07 10:42:24 -0500

    Add configuration for unit and integration test

--- src/test/java/co/edu/unisabana/example/ExampleApplicationTests.java
+++ src/test/java/co/edu/unisabana/example/ExampleApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.unisabana.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class ExampleApplicationTests {
 
 	@Test
